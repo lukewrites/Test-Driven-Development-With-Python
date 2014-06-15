670ffefdfd2b1fe4c25166fd4ccebb3d4e192301
Luke Petschauer <lpetschauer@gmail.com>
2014-06-15 09:24:43 +0800

    playing with views

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page  # this view hasn't been written
 
-# Create your tests here.
-class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home(self):
+        found = resolve('/')  # internal django function to resolve urls
+        self.assertEqual(found.func, home_page)
