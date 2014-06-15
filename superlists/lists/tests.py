0f8fadcd0cd0e2870a1c4f24fa84b50377fc55c1
Luke Petschauer <lpetschauer@gmail.com>
2014-06-15 09:43:12 +0800

    working home_page view, new test
    test (broken) for html on homepage

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from lists.views import home_page  # this view hasn't been written
 
 
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home(self):
         found = resolve('/')  # internal django function to resolve urls
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        # we use b'...' because response.context is in bytes, not a string.
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
