8cdad52e29b603b5029680b9d9408b6cbca9eb44
Luke Petschauer <lpetschauer@gmail.com>
2014-06-21 13:12:21 +0800

    end of ch 6 update

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,13 +1,7 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(.+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 )
